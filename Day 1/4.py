0fd41af33b6d5c626683738542d20f65c3cafcd4
alves-Moises <moisesinho_ra@hotmail.com>
2022-01-06 00:29:36 -0300

    update: lession 4

---
+++ Day 1/4.py
@@ -0,0 +1,4 @@
+print('Day 1 - String Manipulation')
+print("Strnig concatenation is done with the '+' sign.")
+print('eg. print("Hello " + "word")')
+print("New lines can be created with a backslash and n.")
