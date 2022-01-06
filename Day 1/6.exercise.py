a090159412415ad06214df2ba68da7d5bbf3260f
alves-Moises <moisesinho_ra@hotmail.com>
2022-01-06 00:29:55 -0300

    update: lession 6

---
+++ Day 1/6.exercise.py
@@ -0,0 +1,10 @@
+print('welcome to the band name generatior.\n')
+
+city =input("Which city did you grou up in?\n")
+
+pet = input('TWhat is the name of a pet?\n')
+
+print('Your band name coud be ' + city + ' ' + pet)
+
+
+
