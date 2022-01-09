e966772d892eb04c708c45f05ace6eb666b499a8
alves-Moises <moisesinho_ra@hotmail.com>
2022-01-09 04:23:03 -0300

    day 6 exercise

---
+++ Day 2/6 Variable manipulation.py
@@ -0,0 +1,12 @@
+print(round(8/3, 4))
+print(type(8 // 3))
+
+result = 4 / 2
+result /= 2
+
+score = 0
+score += 1
+height = 1.8
+isWinning = True
+
+print(f"Your score is {score}, your height is {height}, you are wining is {isWinning}")
