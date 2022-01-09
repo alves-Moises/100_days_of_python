770a60d4e2e2c65ffef3aec8f6ffcb6884214423
alves-Moises <moisesinho_ra@hotmail.com>
2022-01-09 04:22:25 -0300

    Day 2 lession 2

---
+++ Day 2/2 Type error, type checking and type conversating.py
@@ -0,0 +1,9 @@
+num_char = len(input("What is your name?"))
+new_num_char = str(num_char)
+print("Your name has " + new_num_char + " characters.")
+
+a = str(123)
+print(type(a))
+
+
+print(str(70) + str(100))
