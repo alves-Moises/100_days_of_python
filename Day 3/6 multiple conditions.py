d78970d8638dcb2d639e69741342e23ae6d59b23
alves-Moises <moisesinho_ra@hotmail.com>
2022-01-10 01:10:04 -0300

    6 multiple conditions aula 3

---
+++ Day 3/6 multiple conditions.py
@@ -0,0 +1,24 @@
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm?"))
+
+if height >= 120:
+    print("You can ride the rollercoaster!")
+    age = int(input("what is your age? "))
+    if age < 12:
+        bill = 5
+        print('Child pay $5.')
+    elif age <= 18:
+        bill = 7
+        print("Youth pay $7.")
+    else:
+        bill = 12
+        print("Adult pay $12.")
+
+    whants_phoyo = input("Do you want to take a photo waken?Y or N.")
+    if whants_phoyo == "Y":
+        bill += 3
+
+    print(f"Your final bill is {bill}")
+
+else:
+    print("Sorry, you have to grouw up taller before you can ride.")
