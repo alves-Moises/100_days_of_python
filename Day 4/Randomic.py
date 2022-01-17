3ab842d22c729c929ff09e26082ca5713a83a4e9
alves-Moises <moisesinho_ra@hotmail.com>
2022-01-17 05:00:23 -0300

    day 4 rand

---
+++ Day 4/Randomic.py
@@ -0,0 +1,11 @@
+import random
+# import my_module:
+# random_intereger = random.randint(1, 10)
+
+print(random.randint(3, 19))
+
+# print(my_module.pi)
+
+
+float_random = random.random()
+print(float_random)
