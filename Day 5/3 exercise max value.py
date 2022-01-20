33d7fe10c212505c952e332c7f796bbb7ec0e214
alves-Moises <moisesinho_ra@hotmail.com>
2022-01-20 19:08:17 -0300

    3 exercuse max value day 5

---
+++ Day 5/3 exercise max value.py
@@ -0,0 +1,17 @@
+# 🚨 Don't change the code below 👇
+student_scores = input("Input a list of student scores ").split()
+for n in range(0, len(student_scores)):
+  student_scores[n] = int(student_scores[n])
+print(student_scores)
+# 🚨 Don't change the code above 👆
+
+#Write your code below this row 👇
+
+max_value = 0
+for value in student_scores:
+    max_value = value if value > max_value else max_value
+
+print(max_value)
+
+
+
