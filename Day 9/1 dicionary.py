582d21a673cfb2f797f371eda36e4678da159b05
alves-Moises <moisesinho_ra@hotmail.com>
2022-01-27 18:59:07 -0300

    1 dictionay aula 9

---
+++ Day 9/1 dicionary.py
@@ -0,0 +1,22 @@
+programming_dicionary = {
+    "Bug": "An error in a program...",
+    "Function": "A piece of code that you can easily call over and over again",
+}
+
+print(programming_dicionary["Bug"])
+
+#adding new itens to dictionary
+programming_dicionary["Loop"] = "te action of doing someting again and again"
+print(programming_dicionary)
+
+empty_dicionary ={}
+
+prigramming_dicionary = {}
+print(programming_dicionary)
+
+programming_dicionary["Bug"] = "A moth in your compuyter"
+print(programming_dicionary)
+
+for key in programming_dicionary:
+    print(key)
+    print(programming_dicionary[key])
