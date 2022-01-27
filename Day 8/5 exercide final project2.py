9e8451f795830257c561716595ace09518dd87df
alves-Moises <moisesinho_ra@hotmail.com>
2022-01-27 16:42:21 -0300

    exercise 5 fixing

--- Day 8/5 exercide final project2.py
+++ Day 8/5 exercide final project2.py
@@ -1,8 +1,5 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-word = input("Type your message:\n").lower()
-shift_n = int(input("Type the shift number:\n"))
 
 #TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
 
@@ -19,8 +16,11 @@ def encrypt(text, shift):
    
     new = ''
     for letter in text:
-        # print(alphabet.index(letter))
-        new += alphabet_c[alphabet.index(letter)]
+        if letter in alphabet:
+            new += alphabet_c[alphabet.index(letter)]
+        else:
+            new += letter
+
     print(new)
 
 def decrypt(text, shift):
@@ -28,15 +28,29 @@ def decrypt(text, shift):
 
     new = ''
     for letter in text:
-        new += alphabet[alphabet_c.index(letter)]
-
+        if letter in alphabet:
+            new += alphabet[alphabet_c.index(letter)]
+        else:
+            new += letter
     print(new)
 
 #TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message. 
+def caesar():
+    from art import logo
+    print(logo)
+    while True:        
+        direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+        word = input("Type your message:\n").lower()
+        shift_n = int(input("Type the shift number:\n"))
 
-if direction == 'encode':
-    encrypt(word, shift_n)
-else:
-    decrypt(word, shift_n)
+        if direction == 'encode':
+            encrypt(word, shift_n)
+        else:
+            decrypt(word, shift_n)
+        
+        guess = input("Do you wanna play again? [y] [n]").lower()
+        if guess == 'n':
+            break
 
-print('asd')
+caesar()
+print("Thanks for play this game!")
